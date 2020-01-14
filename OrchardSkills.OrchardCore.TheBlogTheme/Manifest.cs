e15c52d287a3305806ad77a7fd7cd5472b69da28
Orchard Skills <Sales@OrchardSkills.com>
2020-01-14 06:46:55 -0700

    Added OrchardSkills.OrchardCore.TheBlogTheme

---
+++ OrchardSkills.OrchardCore.TheBlogTheme/Manifest.cs
@@ -0,0 +1,9 @@
+using OrchardCore.DisplayManagement.Manifest;
+
+[assembly: Theme(
+    Name = "The Orchard Skills Blog Theme",
+    Author = "Orchard Skills",
+    Website = "https://OrchardSkills.com",
+    Version = "2.0.0",
+    Description = "An Orchard Skills theme adapted for blogs."
+)]
